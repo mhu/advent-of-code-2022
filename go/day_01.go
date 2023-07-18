a2e2872f439f3df7e6838631963c5216d10ee3b8
mhu <marcel.huismann@protonmail.ch>
2023-07-18 16:17:32 -0400

    added Go solution for day 1

---
+++ go/day_01.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+    input, err := os.ReadFile("../input.txt")
+
+    if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(input), "\n")
+	packets := make([]int, 1)
+	index := 0
+
+	for _, line := range lines {
+		if line == "" {
+			packets = append(packets, 0)
+			index += 1
+			continue
+		}
+
+		calories, _ := strconv.Atoi(line)
+		packets[index] += calories
+	}
+
+	sort.Ints(packets)
+
+	fmt.Println(packets[len(packets)-1])
+	fmt.Println(packets[len(packets)-1] + packets[len(packets)-2] + packets[len(packets)-3])
+}
---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/mhu/advent-of-code-2022/go
+
+go 1.18
